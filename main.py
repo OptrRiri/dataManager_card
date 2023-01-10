e6bd804d847e666360187b6968fed2be78a4c073
OptrRiri <roy.oliver.yeo@gmail.com>
2023-01-10 17:31:23 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+# For VSCode, Ctrl+K, then Ctrl+J to unfold all; Ctrl+K, then Ctrl+0 to collapse all
+print("\n\n\n\n-------------------------------------------------------------------------------------------")
+if True: # --- Imports ---
+    '''some usual imported stuff
+        os, sys, random, winsound, moviepy, requests, webbrowser, pyttsx3, bs4, time, shelve, pyperclip, tkinter,
+        keyboard, pygame, pathlib, PIL, moviepy, math, bs4, openpyxl, decimal, mysql, pyglet, ctypes, re, json,
+        pprint, pydavinci, cv2, pynput, pymiere
+    '''
+
+    '''modded imports: 
+        import mysql.connector
+
+        import tkinter as tk
+        import tkinter.tkk as tkk
+        import keyboard as k
+        import pygame as pg
+        import moviepy.editor as mpe
+        import cv2 as cv
+        import pyautogui as pygui
+        import pygetwindow as pywin
+        import numpy as np
+
+        from pathlib import Path
+        from PIL import Image, ImageTk, ImageGrab
+        from decimal import Decimal as d
+        from os.path import isfile, join
+        from pyglet.font.ttf import TruetypeInfo
+        from pydavinci import davinci # only works if pip installed on 3.8.x, and using 3.6.x interpreter
+        from pprint import pprint
+        from pygetwindow._pygetwindow_win import Win32Window
+        from tkinter import filedialog
+    '''
+    import os
+
+    from pathlib import Path
+
+if True: # --- Classes --- (CamelCase)
+    pass
+
+if True: # --- Functions --- (lower_case)
+    pass
+
+if True: # - Global Constants - (UPPER_CASE)
+    FOLDER_OF_THIS_FILE = Path(os.path.dirname(os.path.realpath(__file__)))
+
+def main():  # --- main --- (calling stuff)
+    pass
+    if True: # - init -
+        pass
+    
+    if True: # - objects - 
+        pass
+    
+    if True: # - initial states -
+        pass
+    
+    if True: # - stuff -
+        pass
+
+# testing --------------------------------------------------------------------
+testing = False
+if testing != True:
+    if __name__ == "__main__":
+        main()
+        pass
+else:
+    pass
